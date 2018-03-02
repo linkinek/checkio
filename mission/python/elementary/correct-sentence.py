4be0dd37dda1a5ac16aabcfa5c3db7aa06934656
Ivan Timofeev <linkinek@live.ru>
2018-03-02 15:34:43 +0400

    Added resolved mission correct-sentence

---
+++ mission/python/elementary/correct-sentence.py
@@ -0,0 +1,16 @@
+def correct_sentence(text: str) -> str:
+    if text[-1] != '.':
+        text = text + '.'
+    return text.replace(text[0], text[0].upper(), 1)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(correct_sentence("greetings, friends"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert correct_sentence("greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("hi") == "Hi."
+    print("Coding complete? Click 'Check' to earn cool rewards!")
