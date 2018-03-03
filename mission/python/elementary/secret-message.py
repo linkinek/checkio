eede9899227e978f0376b09d6e2b6586be9657c4
Linkinek <Linkinek@live.ru>
2018-03-03 21:18:38 +0400

    Added resolved mission secret-message

---
+++ mission/python/elementary/secret-message.py
@@ -0,0 +1,9 @@
+def find_message(text):
+    return ''.join(char for char in text if char.isupper())
+
+
+if __name__ == '__main__':
+    assert find_message("How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
+    assert find_message("hello world!") == "", "Nothing"
+    assert find_message("HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
