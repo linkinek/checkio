668bf4f3f3d301b08c09e092ca05d3d17f0a5328
Linkinek <Linkinek@live.ru>
2018-03-03 11:21:16 +0400

    Added resolved mission popular-words

---
+++ mission/python/elementary/popular-words.py
@@ -0,0 +1,28 @@
+def popular_words(text, words):
+    value = {}
+    lower_text = text.lower()
+    for word in words:
+        value[word] = lower_text.count(word)
+    return value
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(popular_words('''
+When I was One,
+I had just begun.
+When I was Two,
+I was nearly new.
+''', ['i', 'was', 'three']))
+
+    assert popular_words('''
+When I was One,
+I had just begun.
+When I was Two,
+I was nearly new.
+''', ['i', 'was', 'three']) == {
+        'i': 4,
+        'was': 3,
+        'three': 0
+    }
+    print("Coding complete? Click 'Check' to earn cool rewards!")
